4f3345f5207c60f1bea21f9f3e0f260c058b646a
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2014-08-09 11:36:36 +0200

    Added "Preferences" item Added an Action bar with "Assemble", "Save" and other buttons Added an Output bar with assembler messages

--- Herguis/Setup.hs
+++ Herguis/Setup.hs
@@ -1,15 +1,19 @@
 module Herguis.Setup(buildInterface) where
 
+import Control.Monad
 import Control.Monad.Trans(liftIO)
 import Data.IORef
+import Data.List
+import Data.Tree
 import Data.Time.Clock
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.SourceView
+import Text.Printf
 
 import qualified Herguis.Action as Action
 import Herguis.Entities
 
-setupMenuItemFile fileStatus window = do
+setupMenuItemFile editorStatusRef assemblerStatusRef config window = do
 	file <- menuItemNewWithMnemonic "_File"
 	menu <- menuNew
 
@@ -17,19 +21,19 @@ setupMenuItemFile fileStatus window = do
 	s1 <- separatorMenuItemNew
 
 	new <- menuItemNewWithMnemonic "_New"
-	new `on` menuItemActivate $ Action.new fileStatus window
+	new `on` menuItemActivate $ Action.new editorStatusRef assemblerStatusRef window
 
 	open <- menuItemNewWithMnemonic "_Open"
-	open `on` menuItemActivate $ Action.open fileStatus window
+	open `on` menuItemActivate $ Action.open editorStatusRef assemblerStatusRef window
 
 	save <- menuItemNewWithMnemonic "_Save"
-	save `on` menuItemActivate $ Action.save fileStatus window >> return ()
+	save `on` menuItemActivate $ Action.save editorStatusRef assemblerStatusRef window >> return ()
 
 	saveas <- menuItemNewWithMnemonic "Save _as"
-	saveas `on` menuItemActivate $ Action.saveAs fileStatus window >> return ()
+	saveas `on` menuItemActivate $ Action.saveAs editorStatusRef assemblerStatusRef window >> return ()
 
 	exit <- menuItemNewWithMnemonic "_Exit"
-	exit `on` menuItemActivate $ Action.quit fileStatus window >>= \x -> return ()
+	exit `on` menuItemActivate $ Action.quit editorStatusRef assemblerStatusRef (lastStatusFile config) window >>= \x -> return ()
 
 	containerAdd menu new
 	containerAdd menu s0
@@ -43,58 +47,146 @@ setupMenuItemFile fileStatus window = do
 
 	return file
 
-setupMenuItemEdit = menuItemNewWithMnemonic "_Edit"
+setupMenuItemPreferences = menuItemNewWithMnemonic "_Preferences"
 setupMenuItemHelp = menuItemNewWithMnemonic "_Help"
 
-setupMenuBar fileStatus window = do
+setupMenuBar editorStatusRef assemblerStatusRef config window = do
 	bar <- menuBarNew
-	miFile <- setupMenuItemFile fileStatus window
-	miEdit <- setupMenuItemEdit
+	miFile <- setupMenuItemFile editorStatusRef assemblerStatusRef config window
+	miPref <- setupMenuItemPreferences
 	miHelp <- setupMenuItemHelp
 
 	containerAdd bar miFile
-	containerAdd bar miEdit
+	containerAdd bar miPref
 	containerAdd bar miHelp
 
 	return bar
 
-setupTextView = do
+setupTextView config = do
 	tTextView <- sourceViewNew
-	set tTextView [textViewWrapMode := WrapWord, sourceViewShowLineNumbers := True]
+	set tTextView [textViewWrapMode := wrapMode config, sourceViewShowLineNumbers := lineNumbering config]
 	tTextBuffer <- get tTextView textViewBuffer
 
 	return (tTextView,tTextBuffer)
 
-buildInterface config = do
+setupActionBar machines editorStatusRef assemblerStatusRef assemblerName msgFile outputList window = do
+	bar <- hBoxNew False 0
+	machineBox <- comboBoxNewText
+	set machineBox [comboBoxWrapWidth := 1]
+	mapM_ (\(x,y) -> comboBoxAppendText machineBox x) machines
+	assemblerStatus <- readIORef assemblerStatusRef
+	when (machineFile assemblerStatus /= "") $
+		let index = find (\((_,y),_) -> y == (machineFile assemblerStatus)) $ zip machines [0,1..] in
+		case index of
+			Just (_,index) -> set machineBox [comboBoxActive := index]
+			Nothing -> return ()
 
-	activeFile <- newIORef $ filename config
-	now <- getCurrentTime
-	updateTime <- newIORef $
-		case lastUpdate config of
-			Nothing -> now
-			Just t -> t
+	sep <- vSeparatorNew
 
-	syncFile <- newIORef $ syncWithFile config
-	modified <- newIORef False
+	refreshImg <- imageNewFromStock stockRefresh IconSizeButton
+	assembleImg <- imageNewFromStock stockExecute IconSizeButton
+	refreshBtn <- buttonNew
+	assembleBtn <- buttonNew
+	set refreshBtn [buttonImage := refreshImg, widgetTooltipText := Just "Refresh"]
+	set assembleBtn [buttonImage := assembleImg, widgetTooltipText := Just "Assemble"]
+
+	boxPackStart bar machineBox PackNatural 0
+	boxPackStart bar sep PackNatural 10
+	boxPackStart bar refreshBtn PackNatural 0
+	boxPackStart bar assembleBtn PackNatural 0
+
+	machineBox `on` changed $ do
+		active <- get machineBox comboBoxActive
+		let (name,location) = machines !! active
+		assemblerStatus <- readIORef assemblerStatusRef
+		writeIORef assemblerStatusRef assemblerStatus{machineFile = location}
+
+	refreshBtn `on` buttonActivated $ do
+		editorStatus <- readIORef editorStatusRef
+		Action.load (filename editorStatus) editorStatusRef
+
+	assembleBtn `on` buttonActivated $ Action.assemble editorStatusRef assemblerStatusRef assemblerName msgFile outputList window
+
+	return bar
+
+setupOutputBar = do
+	treeModel <- listStoreNew []
+	treeView <- treeViewNewWithModel treeModel
+	vAdjust <- get treeView treeViewVAdjustment
+	case vAdjust of
+		Just ad -> set ad [adjustmentUpper := 0.3, adjustmentValue := 0.1]
+		Nothing -> return ()
+
+	typeCol <- treeViewColumnNew
+	lineCol <- treeViewColumnNew
+	msgCol <- treeViewColumnNew
+	treeViewColumnSetTitle typeCol "Type"
+	treeViewColumnSetTitle lineCol "Line"
+	treeViewColumnSetTitle msgCol "Message"
+	typeRenderer <- cellRendererTextNew
+	lineRenderer <- cellRendererTextNew
+	msgRenderer <- cellRendererTextNew
+	cellLayoutPackStart typeCol typeRenderer False
+	cellLayoutPackStart lineCol lineRenderer False
+	cellLayoutPackStart msgCol msgRenderer False
+	cellLayoutSetAttributes typeCol typeRenderer treeModel
+		$ \(a,b,c) -> [cellText := a]
+	cellLayoutSetAttributes typeCol lineRenderer treeModel
+		$ \(a,b,c) -> [cellText := b]
+	cellLayoutSetAttributes msgCol msgRenderer treeModel
+		$ \(a,b,c) -> [cellText := c]
+	treeViewAppendColumn treeView typeCol
+	treeViewAppendColumn treeView lineCol
+	treeViewAppendColumn treeView msgCol
+
+	treeViewSetHeadersVisible treeView True
+
+	vadj <- get treeView treeViewVAdjustment
+	hadj <- get treeView treeViewHAdjustment
+
+	swin <- scrolledWindowNew hadj vadj
+	containerAdd swin treeView
+
+	return (treeModel,swin)
+
+
+buildInterface config editorStatus assemblerStatus = do
 
 	window  <- windowNew
 	mainBox <- vBoxNew False 0
 	textWindow <- scrolledWindowNew Nothing Nothing
 	set textWindow [scrolledWindowHscrollbarPolicy := PolicyAutomatic, scrolledWindowVscrollbarPolicy := PolicyAutomatic]
 
-	(tTextView,tTextBuffer) <- setupTextView
-	textBufferInsertAtCursor tTextBuffer (text config)
-	tTextBuffer `on` bufferChanged $ liftIO $ writeIORef modified True
+	-- sets up editor's text
+	(tTextView,tTextBuffer) <- setupTextView config
+	editorStatusRef <- newIORef editorStatus{buffer = tTextBuffer}
+	Action.load (filename editorStatus) editorStatusRef
+	tTextBuffer `on` bufferChanged $ liftIO $ do
+		eStatus <- readIORef editorStatusRef
+		writeIORef editorStatusRef eStatus{modified = True}
 
-	let fileStatus = FileStatus{filenameRef = activeFile,lastUpdateRef = updateTime, buffer = tTextBuffer, syncRef = syncFile, modifiedRef = modified}
-	mMenuBar <- setupMenuBar fileStatus window
+	assemblerStatusRef <- newIORef assemblerStatus
+
+	(outputList, outputView) <- setupOutputBar
+
+	mMenuBar <- setupMenuBar editorStatusRef assemblerStatusRef config window
+
+	actionBar <- setupActionBar (machines config) editorStatusRef assemblerStatusRef (assembler config) (messageFile config) outputList window
 
 	set window [windowDefaultWidth := 640, windowDefaultHeight := 480, containerChild := mainBox, containerBorderWidth := 1]
+
+	let cols = 4
+	sourceBox <- tableNew cols 1 True
 	boxPackStart mainBox mMenuBar PackNatural 0
-	boxPackStart mainBox textWindow PackGrow 0
+	boxPackStart mainBox actionBar PackNatural 0
+	boxPackStart mainBox sourceBox PackGrow 0
+	tableAttachDefaults sourceBox textWindow 0 1 0 (cols -1)
+	tableAttachDefaults sourceBox outputView 0 1 (cols -1) cols
 	containerAdd textWindow tTextView
 
-	window `on` deleteEvent $ Action.quit fileStatus window
-	window `on` focusInEvent $ liftIO $ Action.reloadFile fileStatus
+	window `on` deleteEvent $ liftIO $ Action.quit editorStatusRef assemblerStatusRef (lastStatusFile config) window
+	window `on` focusInEvent $ liftIO $ Action.reloadFile editorStatusRef
 
 	widgetShowAll window
+
+	when (hasError config) $ Action.popupError (errorMsg config) window
